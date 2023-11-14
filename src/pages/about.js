7c7b1438d42cad4aa90566e1fd0e5f7655f5799b
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-11-14 12:17:28 +0900

    About Page 생성

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>Hi there I'm proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
+}
+
+export const Head = () => (
+  <>
+    <title>About ME</title>
+    <meta name="description" content="Your description" />
+  </>
+)
+
+export default AboutPage
