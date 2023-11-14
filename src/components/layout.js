818e58ea69c855db317039a598503b96a32d5a27
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-11-14 16:25:42 +0900

    Link 컴포넌트까지 연결 완료

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,51 +1,24 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
- */
+import { Link } from "gatsby"
+import React from "react"
 
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
+const Layout = ({ pageTitle, children }) => {
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/aboutme">About Me </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
   )
 }
-
 export default Layout
---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const BlogPage = () => {
+  return (
+    <main>
+      <h1>Hi Im BlogPAge</h1>
+    </main>
+  )
+}
+export const Head = () => <title>Blog</title>
+export default BlogPage
