63cb076e337ad8f14ec82fdc5ee1bc65f49698a7
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-11-27 14:12:58 +0900

    [ADD] Post Category Page 구현

--- src/components/PostSearch.jsx
+++ src/components/PostSearch.jsx
@@ -24,13 +24,19 @@ const StPostSearchWrapper = styled.div`
     border-top: 0;
     border-left: 0;
     border-right: 0;
-    border-bottom: 2px solid var(--accentColor);
+    border-bottom: 4px solid var(--accentColor);
     background-color: transparent;
     padding-bottom: 0.5rem;
+    transition: all 0.2s ease;
+    opacity: 0.5;
+  }
+  input:focus {
+    opacity: 1;
   }
   svg {
     width: 1.5rem;
     height: 1.5rem;
+    cursor: pointer;
   }
 `
 
